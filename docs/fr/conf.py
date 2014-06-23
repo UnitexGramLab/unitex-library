efe25edd986b35a63b2334fadd87444587ff20bd
martinec <me@martinec.org>
2014-06-23 22:35:23 +0200

    add rtd flag check

--- docs/fr/conf.py
+++ docs/fr/conf.py
@@ -15,6 +15,10 @@
 import sys
 import os
 
+# When RTD builds your project, it sets the READTHEDOCS environment variable
+# to the string True. 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
